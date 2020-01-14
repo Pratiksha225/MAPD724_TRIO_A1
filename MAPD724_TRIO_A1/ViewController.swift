d2058a5ced83b3a8c68f89cea4ae5768e2508693
pratiksha kathiriya <pratikshakakathiriya22597@gmail.com>
2020-01-14 12:01:57 -0500

    Initial Commit

---
+++ MAPD724_TRIO_A1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD724_TRIO_A1
+//
+//  Created by Pratiksha Kathiriya on 2020-01-14.
+//  Copyright © 2020 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
